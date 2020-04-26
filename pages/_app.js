6459c4f43caf12465a49715c04b177322aa61f59
TimeEmit <LiamNorris1231853211@gmail.com>
2020-04-25 18:17:40 -0700

    Semicolons

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import (require('purecss').getFile('pure-min.css'));
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;

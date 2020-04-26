2db9369a3bb442e25199aed099535cb5be7bc6b8
TimeEmit <LiamNorris1231853211@gmail.com>
2020-04-25 18:18:57 -0700

    Revert "Simplify layout"
    This reverts commit a7f426045f706a706b9d5680ecc12fe92d52fedf.

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,25 @@ import React from 'react';
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <header>
+        <nav>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          |
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+          |
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </nav>
+      </header>
+
       <Component {...pageProps} />
+
+      <footer>I`m here to stay</footer>
     </>
   );
 }

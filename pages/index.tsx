50069db6eec52966e1a70c20a16159c55a1660ce
TimeEmit <LiamNorris1231853211@gmail.com>
2021-11-04 02:24:06 -0700

    Favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,17 @@
 import React from "react";
 
 import styles from "../styles/Home.module.scss";
-import PageWithNavigator from "../components/PageWithNavigator";
+import Page from "../components/Page";
 
 const Index = () => {
   return (
-    <main className="pure-u-1">
-      <h1 className="header centered-text">The Socratic Garden</h1>
-      <img className={"pure-img centered " + styles.pulsingLogo} src="/logo-no-text.png" />
-      <h1 className="header centered-text">Is Under Development</h1>
-    </main>
+		<Page title="Socratic Garden">
+			<main className="pure-u-1">
+				<h1 className="header centered-text">The Socratic Garden</h1>
+				<img className={"pure-img centered " + styles.pulsingLogo} src="/logo-no-text.png" />
+				<h1 className="header centered-text">Is Under Development</h1>
+			</main>
+		</Page>
   );
 };
 

fe2e42c33d8cb9b6ef49b388e78ece4c41c1f2cc
TimeEmit <LiamNorris1231853211@gmail.com>
2021-11-04 02:14:02 -0700

    Purge index.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,8 @@ import React from "react";
 
 import styles from "../styles/Home.module.scss";
 import PageWithNavigator from "../components/PageWithNavigator";
-import { LessonFetch } from "../models/Lesson";
-import { QuestionByLessonID } from "../models/Question";
-import ChallengeView from "../components/ChallengeView";
 
 const Index = () => {
-  const lesson = LessonFetch(0);
-  const question = QuestionByLessonID(0);
   return (
     <main className="pure-u-1">
       <h1 className="header centered-text">The Socratic Garden</h1>

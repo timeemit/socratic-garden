0c3b65b88131ae991b10f6ed96da03dbd1ade3cf
TimeEmit <LiamNorris1231853211@gmail.com>
2021-12-01 21:05:13 -0800

    Goth girl

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,8 @@ const Index = () => {
 				<h1 className="header centered-text">The Socratic Garden</h1>
 				<img className={"pure-img centered " + styles.pulsingLogo} src="/logo-no-text.png" />
 				<h1 className="header centered-text">Is Under Development</h1>
+        <img className={"pure-img centered " + styles.goth} src="/patrick/goth_stares_out_window-no_window.png" />
+				<h1 className="header centered-text">Thanks for Your Patience</h1>
 			</main>
 		</Page>
   );

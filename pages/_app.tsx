8efcbc7050009c7cd907d0d9aed545186779a5e6
TimeEmit <LiamNorris1231853211@gmail.com>
2021-12-01 21:05:48 -0800

    Comment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -41,6 +41,8 @@ function MyApp({ Component, pageProps, csrfToken }) {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* Favicons */}
 				<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
 				<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
@@ -48,7 +50,6 @@ function MyApp({ Component, pageProps, csrfToken }) {
 				<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
 				<meta name="msapplication-TileColor" content="#da532c" />
 				<meta name="theme-color" content="#ffffff" />
-
       </Head>
 
       <Component {...pageProps} />

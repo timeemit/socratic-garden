e0e384f569e827b93651e1536092e0109c6473e1
TimeEmit <LiamNorris1231853211@gmail.com>
2021-12-04 10:18:20 -0800

    PageWithNavigator.tsx

--- components/PageWithNavigator.tsx
+++ components/PageWithNavigator.tsx
@@ -4,6 +4,15 @@ import Page, { Props } from "./Page";
 
 const PageWithNavigator = (
   { children, title } // Moved the Navigator to the App component
-) => <Page title={title}>{children}</Page>;
+) => (
+  <Page title={title} grid={true}>
+    <nav className="pure-u-1-8">
+      Navigation
+    </nav>
+    <main className="pure-u-7-8">
+      {children}
+    </main>
+  </Page>
+);
 
 export default PageWithNavigator;

42c17f0e4fbef6a7ed2230705b1906a44f6f42ca
TimeEmit <LiamNorris1231853211@gmail.com>
2021-12-03 22:01:10 -0800

    Treehouse

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,9 @@ const Index = () => {
 				<h1 className="header centered-text">The Socratic Garden</h1>
 				<img className={"pure-img centered " + styles.pulsingLogo} src="/logo-no-text.png" />
 				<h1 className="header centered-text">Is Under Development</h1>
-        <img className={"pure-img centered " + styles.goth} src="/patrick/goth_stares_out_window-no_window.png" />
+        <img className={"pure-img centered " + styles.goth} src="/patrick/treehouse.png" />
 				<h1 className="header centered-text">Thanks for Your Patience</h1>
+        <img className={"pure-img centered " + styles.goth} src="/patrick/goth_stares_out_window-no_window.png" />
 			</main>
 		</Page>
   );

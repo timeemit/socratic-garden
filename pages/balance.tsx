6b88cc566f5af2636301739774f81964fa3f63a5
TimeEmit <LiamNorris1231853211@gmail.com>
2021-12-04 13:04:49 -0800

    Balance

--- pages/balance.tsx
+++ pages/balance.tsx
@@ -1,9 +1,12 @@
 // @format
 import React from "react";
+import classnames from "classnames";
 
 import PageWithNavigator, {
   CurrentPage,
 } from "../components/PageWithNavigator";
+
+import styles from "../styles/Balance.module.scss";
 
 const Balance = () => {
   return (
@@ -11,7 +14,23 @@ const Balance = () => {
       title="Socratic Garden: My Balance"
       current={CurrentPage.Balance}
     >
-      Balance Details
+      <h1>My Balance</h1>
+      <em>
+        Earn coins by evaluating peer submissions to challenges you have already
+        completed
+      </em>
+
+      <div className={classnames("pure-g", styles.actions)}>
+        <div className="pure-u-1-3 centered-text">
+          <button className="pure-button">Transfer Coins</button>
+        </div>
+        <div className="pure-u-1-3 centered-text">
+          <button className="pure-button">Buy Coins</button>
+        </div>
+        <div className="pure-u-1-3 centered-text">
+          <button className="pure-button">Sell Coins</button>
+        </div>
+      </div>
     </PageWithNavigator>
   );
 };
